e12fe91cc51092444fe4b34ead767dda91902d04
caiooww <caiomksp@gmail.com>
2025-03-14 22:12:16 -0300

    sim

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -18,6 +18,11 @@ public class Product {
     }
 
     public String toString() {
-        return name + ", $ " + String.format("%.2f", price) + ", " + quantity + " units, Total: $ " + String.format("%.2f", totalValueInStock());
+        return name
+                + ", $ "
+                + String.format("%.2f", price)
+                + ", " + quantity
+                + " units, Total: $ "
+                + String.format("%.2f", totalValueInStock());
     }
 }
